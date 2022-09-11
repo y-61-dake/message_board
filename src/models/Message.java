c67732fabc1dcd590c1b0125696eb2503eea1e89
y-61- <y-61-@192.168.11.63>
2022-09-12 05:10:43 +0900

    getAllMessagesを追加

--- src/models/Message.java
+++ src/models/Message.java
@@ -1,4 +1,4 @@
-package model;
+package models;
 
 import java.sql.Timestamp;
 
@@ -7,9 +7,18 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
 import javax.persistence.Table;
 
 @Entity
+@NamedQueries({
+    @NamedQuery(
+        name = "getAllMessages",
+        query = "SELECT m FROM Message AS m ORDER BY m.id DESC"
+    )
+
+})
 @Table(name = "message")
 public class Message {
     @Id
